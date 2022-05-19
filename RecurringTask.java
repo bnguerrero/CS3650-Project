50f02e55e3c5f514f2880e3e823ee360a58ceb95
Aseward1005 <105628085+Aseward1005@users.noreply.github.com>
2022-05-18 17:51:45 -0700

    Update RecurringTask.java
    made the code in the constructor a bit more concise

--- RecurringTask.java
+++ RecurringTask.java
@@ -7,16 +7,16 @@ public class RecurringTask extends Task {
     private ArrayList<TransientTask> tasks; //A recurring task is a collection of transient tasks
     
     public RecurringTask(String name, int date, Runtime runtime, int endDate, int frequency) throws IllegalArgumentException {
+        super(name, date, runtime);
+        
         //check that the frequency is a 1 or a 7
         if (frequency != 1 && frequency != 7)
             throw IIllegalArgumentException("Frequency must be a 1 or a 7"); //this may be better handled in main, but for purposes of this project we don't really have time to discuss that.
-        
-        this.name = name;
-        this.type = type.RECURRING;
-        this.date = date;
-        this.runtime = runtime;
-        this.endDate = endDate;
         this.frequency = frequency;
+        
+        this.type = type.RECURRING;
+        this.endDate = endDate;
+        
         instantiateTasks();
     }
     
