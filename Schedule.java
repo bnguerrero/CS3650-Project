4e21da16698f55ad6bf55c083e136d2e9cfb2d5b
Imaad Mohammed <imaadmohammed@gmail.com>
2022-05-20 15:31:48 -0700

    fixed conflict merge

--- Schedule.java
+++ Schedule.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import javax.management.InvalidAttributeValueException;
+
 public class Schedule {
     public ArrayList<Task> taskList;
 
@@ -47,7 +49,7 @@ public class Schedule {
     }
 
     public void editTask(Task task, String newName, int newDate, Runtime newRuntime, boolean antiTaskVal,
-            int newEndDate) {
+            int newEndDate) throws InvalidAttributeValueException, IllegalArgumentException, javax.naming.directory.InvalidAttributeValueException {
         Type taskType = task.getType();
         int taskFreq = task.getFrequency();
         deleteTask(task);
@@ -116,14 +118,15 @@ public class Schedule {
                         return false;
                     }
                 }
-            }
-            
-            //if it is a transient task, check the 2 tasks
-            else if (task.getType() == type.TRANSIENT){
-                if(!task.hasAntiTask()) //skip this if the task has an anti task.
-                    if (task.getDate() == newTask.getDate()) //if the dates are the same, check their runtimes
-                        if (!conflictHelper(t.getRuntime(), newTask.getRuntime()))
+                //for each recurring task t of task, if t is not an ant-task, check for conflicts
+                for (Task t : task.getTasks()) {
+                    if(!t.hasAntiTask()) {
+                        if(!conflictHelper(newTask.getDate(), task.getDate(), newTask.getRuntime(), task.getRuntime())) {
                             return false;
+                        }
+                    }
+                }
+
             }
             //if a task in taskList is transient, check for conflicts 
             else if(task.getType().equals(Type.TRANSIENT)) {
@@ -135,141 +138,77 @@ public class Schedule {
             }
             else {
                 throw new IllegalArgumentException("Invalid task type for conflict checking.");
-         }return true;
+            }
 
+        }
+
+        return true; 
     }
-
-    // checks the runtimes, if they overlap, this says false.
-    // this currently doesn't consider tasks that wrap past midnight, as it only
-    // checks for things that start on the same day.
-    private boolean conflictHelper(Runtime r1, Runtime r2){
-        //first order by start time.
-        //first is the earlier one
-        if (r1.startTime > r2.startTime){
-            first = r2;
-            second = r1;
-        }
-        else if (r1.startTime < r2.startTime)
-        { 
-            first = r1;
-            second = r2;
-        }
-        else
-            return false; //it is impossible for them to start at the same time and not overlap.
         
-        //then define their endpoints
-        double firstend = first.startTime + first.duration;
-        //double secondend = second.startTime + second.duration; //it doesn't matter when the second starts
-         
-        //the first will always start before the second.
-        //if the first ends after the second starts, no matter what that is an overlap.
-        //if the first ends before the second starts, no matter what, that will not be an overlap.
-        if (firstend <= second.startTime)
-            return true;
-        else
-            return false;
-    //if the type is recurring, recursively check.
-    //call checkConflicts on each transient task contained in the recurring task.
-    //if there is an overlap at any point, this will return false.
-    //imperceptible bug: this will check each task in the recurring task's name
-    //as well. Small optimization issue
-    if (newTask.getType() == type.RECURRING){
-    for (t : newTask.getTasks()){
-    if (!checkConflicts(t))
-    return false;
-    }
-    }
 
-    //check the conflicts between the newly added task and every other task in
-    the list.
-    for(task : taskList) {
-    //first check name conflicts
-    if (newTask.getName == task.getName())
-    return false;
-
-    //then check timing conflicts
-    if (task.getType() == type.RECURRING) //if it is a recurring task, look
-    through the set{
-    for (t : task.getTasks()){
-    if(!task.hasAntiTask()) //skip this if the task has an anti task.
-    if (t.getDate() == newTask.getDate()) //if the dates are the same, check
-    their runtimes
-    if(!task.hasAntiTask()) //skip this if the task has an anti task.
-    if(!conflictHelper(t.getRuntime, newTask.getRuntime))
-    return false;
-    }
-    }
-
-    //if it is a transient task, check the 2 tasks
-    else if (task.getType() == type.TRANSIENT){
-    if(!task.hasAntiTask()) //skip this if the task has an anti task.
-    if (task.getDate() == newTask.getDate()) //if the dates are the same, check
-    their runtimes
-    if (!conflictHelper(t.getRuntime, newTask.getRuntime))
-    return false;
-    }
-
-    //in case the type superclass is thrown into here, since its basically the
-    same as a transient class it actually would work
-    //but since there may be future subclasses without implementation, this was
-    safer.
-    else
-    throw new IllegalArgumentException("Invalid task type for conflict
-    checking.");
-    }
-    return true;
-    }
-
+   
     //checks the runtimes, if they overlap, this says false.
-    //this currently doesn't consider tasks that wrap past midnight, as it only
-    checks for things that start on the same day.
+    //this currently doesn't consider tasks that wrap past midnight, as it only checks for things that start on the same day.
+    private boolean conflictHelper(int date1, int date2, Runtime r1, Runtime r2) {
+        if(date1 == date2) {
+            double endTime1 = r1.startTime + r1.duration;
+            double endTime2 = r2.startTime + r2.duration;
 
-    private boolean conflictHelper(Runtime r1, Runtime r2){
-    //first order by start time.
-    //first is the earlier one
-    if (r1.startTime > r2.startTime){
-    first = r2;
-    second = r1;
-    }
-    else if (r1.startTime < r2.startTime)
-    {
-    first = r1;
-    second = r2;
-    }
-    else
-    return false; //it is impossible for them to start at the same time and not
-    overlap.
-
-    //then define their endpoints
-    double firstend = first.startTime + first.duration;
-    //double secondend = second.startTime + second.duration; //it doesn't matter
-    when the second starts
-
-    //the first will always start before the second.
-    //if the first ends after the second starts, no matter what that is an
-    overlap.
-    //if the first ends before the second starts, no matter what, that will not
-    be an overlap.
-    if (firstend <= second.startTime)
-    return true;
-    else
-    return false;
+            // starts at the same time or in the middle of an already existing event
+            if(r1.startTime >= r2.startTime && r1.startTime < endTime2) {
+                return false;
+            }
+            // ends at the same time or in the middle of an already existing event
+            else if(endTime1 > r2.startTime && endTime1 <= endTime2) {
+                return false;
+            }
+             // starts before an already existing event but ends after it
+            else if(r1.startTime <= r2.startTime && endTime1 >= endTime2) {
+                return false;
+            }
+        }
+        return true;
+        
+        // //first order by start time.
+        // //first is the earlier one
+        // if (r1.startTime > r2.startTime){
+        //     first = r2;
+        //     second = r1;
+        // }
+        // else if (r1.startTime < r2.startTime)
+        // { 
+        //     first = r1;
+        //     second = r2;
+        // }
+        // else
+        //     return false; //it is impossible for them to start at the same time and not overlap.
+        
+        // //then define their endpoints
+        // double firstend = first.startTime + first.duration;
+        // //double secondend = second.startTime + second.duration; //it doesn't matter when the second starts
+         
+        // //the first will always start before the second.
+        // //if the first ends after the second starts, no matter what that is an overlap.
+        // //if the first ends before the second starts, no matter what, that will not be an overlap.
+        // if (firstend <= second.startTime)
+        //     return true;
+        // else
+        //     return false;
     }
 
     // public void writeSchedule(String filename) {
-    // JsonWriter writer = new FileWriter();
-    // writer.write(taskList, filename);
+    // FileWriter writer = new FileWriter();
+    // writer.write(taskList);
     // }
 
     // public void readSchedule(String filename) {
     // FileReader reader = new FileReader(this);
     // reader.read(filename);
     // }
-    
-    // public void writeScheduleDuration(String filename, int startDate, int endDate) {
-    // JsonWriter writer = new FileWriter(startDate, endDate);
-    // writer.write(taskList, filename);
-    // }
+
+    // public void writeScheduleDuration(String filename, int duration) {
+
+
 }
 
 // //check the conflicts between the newly added task and every other task in the list.
