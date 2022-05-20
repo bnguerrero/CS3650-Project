a3c9bfe86c60647f856fee61ed527a53c7f80488
Aseward1005 <105628085+Aseward1005@users.noreply.github.com>
2022-05-20 02:59:59 -0700

    Update RecurringTask.java

--- RecurringTask.java
+++ RecurringTask.java
@@ -8,6 +8,7 @@ public class RecurringTask extends Task {
         super(name, date, runtime);
 
         // check that the frequency is a 1 or a 7
+        // 1 is daily, 7 is weekly
         if (frequency != 1 && frequency != 7)
             throw new IllegalArgumentException("Frequency must be a 1 or a 7"); // this may be better handled in main, but
                                                                              // for purposes of this project we don't
@@ -18,6 +19,16 @@ public class RecurringTask extends Task {
 
         instantiateTasks();
     }
-
+    
+    private void instantiateTasks() {
+    
+        switch(frequency)
+        {
+            case 1:
+                break;
+            case 7:
+                break;
+        }
+    }
 
 }
